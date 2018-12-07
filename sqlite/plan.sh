8983b364462662ac04ea619ece087f47d89268e5
Mandi Walls <mandi.walls@gmail.com>
2018-12-07 15:08:06 +0000

    Updating sqlite

--- sqlite/plan.sh
+++ sqlite/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=sqlite
-pkg_version=3130000
-pkg_origin=core
+pkg_version=3260000
+pkg_origin=lnxchk
 pkg_license=('Public Domain')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A software library that implements a self-contained, serverless, zero-configuration, transactional SQL database engine."
@@ -8,7 +8,7 @@ pkg_upstream_url=https://www.sqlite.org/
 pkg_source=https://www.sqlite.org/2016/${pkg_name}-autoconf-${pkg_version}.tar.gz
 pkg_filename=${pkg_name}-autoconf-${pkg_version}.tar.gz
 pkg_dirname=${pkg_name}-autoconf-${pkg_version}
-pkg_shasum=e2797026b3310c9d08bd472f6d430058c6dd139ff9d4e30289884ccd9744086b
+pkg_shasum=9af2df1a6da5db6e2ecf3f463625f16740e036e9
 pkg_deps=(core/glibc core/readline)
 pkg_build_deps=(core/gcc core/make core/coreutils)
 pkg_lib_dirs=(lib)
